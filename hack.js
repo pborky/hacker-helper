b2b61c3de5a5ec071831588f3928acb71b0d6066
Peter Boráros <pborky@pborky.sk>
2013-07-13 23:48:28 +0200

    add some gems

--- hack.js
+++ hack.js
@@ -1,7 +1,13 @@
 
-console.log('*** This is grandma\'s hack. ***')
-console.log('jQuery("h1").text() -> ' + jQuery('h1').text())
-console.log('document.getElementsByTagName("h1")[0].innerText -> ' + document.getElementsByTagName('h1')[0].innerText)
+console.log('*** This is grandma\'s hack. ***');
 
-jQuery('h1').text('You are such a dick.')
+if (jQuery('h1')) {
+    console.log('jQuery("h1").text() -> ' + jQuery('h1').text());
+    console.log('document.getElementsByTagName("h1")[0].innerText -> ' + document.getElementsByTagName('h1')[0].innerText);
+    jQuery('h1').text('You are such a dick.');
+}
 
+if ($('h2')) $('h2').text('trololo');
+if ($('h3')) $('h3').text('fooka');
+
+//jQuery('form').submit()
